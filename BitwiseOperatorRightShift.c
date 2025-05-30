79d966690f2f2ebbc683c4297667d3cd10002566
Tridib Banik <tridib.perfect@gmail.com>
2025-05-30 13:51:12 -0400

    Example of bitwise operator Right Shift

---
+++ BitwiseOperatorRightShift.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+  i >> pos = i times 2 to the power of pos
+*/
+int main() {
+  int i = 7; // 10000
+  int pos = 2; // pos = 2
+  /*
+    10000 >> 2 = 100
+  */
+  int k = i >> pos; // 100 corresponds to 16 divided by 2 to the power of 2 = 4 in decimal.
+  printf("%d ^ %d = %d\n", i, pos, k);
+  printf("Address of int i = %p\n", (void *)&i);
+  printf("Address of int j = %p\n", (void *)&pos);
+  printf("Address of int k = %p\n", (void *)&k);
+  return 0;
+}
