1770aab7ebce20b6d43993eb2a7d0954ceac863f
Tridib Banik <tridib.perfect@gmail.com>
2025-06-01 13:07:18 -0400

    Example of switch statement

--- SwitchStatement.c
+++ SwitchStatement.c
@@ -2,14 +2,44 @@
 
 int main() {
   int i = 1, j = 2;
-  switch (i * j) {
-    case 1: printf("%d * %d = %d", i, j, i*j);
+  switch (i + j) {
+    case 1: printf("one");
     break;
-    case 2: printf("%d * %d = %d", i, j, i*j);
+    case 2: printf("two");
     break;
-    case 3: printf("%d * %d = %d", i, j, i*j);
+    case 3: printf("three");
     break;
     default: printf("The result is not 1, 2, or 3.");
-    return 0;
   }
+  printf("\n--------\n");
+  switch (i * j) {
+    case 1: printf("one");
+    break;
+    case 2: printf("two");
+    break;
+    case 3: printf("three");
+    break;
+    default: printf("The result is not 1, 2, or 3.");
+  }
+  printf("\n--------\n");
+  switch (i / j) {
+    case 1: printf("one");
+    break;
+    case 2: printf("two");
+    break;
+    case 3: printf("three");
+    break;
+    default: printf("The result is not 1, 2, or 3.");
+  }
+  printf("\n--------\n");
+  switch (i) {
+    case 1: printf("one");
+    break;
+    case 2: printf("two");
+    break;
+    case 3: printf("three");
+    break;
+    default: printf("The result is not 1, 2, or 3.");
+  }
+  return 0;
 }
