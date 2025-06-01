2c9dcedb2eadc9c4bf208c5290fcc6c370bfc0dc
Tridib Banik <tridib.perfect@gmail.com>
2025-06-01 14:14:54 -0400

    Example of For Loop

---
+++ ForLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  // For loop to compute the factorial of a positive integer.
+  int i, factorial = 1;
+  printf("Enter a positive integer: ");
+  scanf("%d", &i);
+  printf("The factorial of %d is ", i);
+  for (int j = 1; j <= i; i--) {
+    factorial *= i;
+  } 
+  printf("%d\n", factorial);
+  return 0;
+}
