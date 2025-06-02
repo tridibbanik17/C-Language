13622d6b9d83f633d1a32c4c0f292cd1d8837a3a
Tridib Banik <tridib.perfect@gmail.com>
2025-06-02 00:47:04 -0400

    Compute factorial of a user input number

---
+++ FactorialFunction.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int factorial(int);
+
+int factorial(int n) {
+  int fact = 1;
+  if (n < 0) {
+    printf("Factorial of a negative integers is not possible.\n");
+    printf("Please enter an integer greater than or equal to 0.\n");
+    return 1;
+  }
+  for (int i = 1; i <= n; i++) {
+    fact = fact*i;
+  }
+  return fact;
+}
+int main() {
+  int n;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+  printf("The factorial of %d is %d.\n", n, factorial(n));
+}
