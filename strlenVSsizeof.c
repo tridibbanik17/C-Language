d0ce04a20f93bad3edeea7c49fa7a2b5fab3f254
Tridib Banik <tridib.perfect@gmail.com>
2026-01-03 23:05:11 -0500

    Difference between the output of strlen() and sizeof() without specifying string size

--- strlenVSsizeof.c
+++ strlenVSsizeof.c
@@ -3,9 +3,12 @@
 
 int main()
 {
-  char str[20];
-  printf("Enter a string : ");
-  gets(str);
+  char str[] = "Hello"; 
+  /////////////////////////////////////////////////////////////
+  // sizeof(str) = strlen() + 1; because of '\0'.
+  /////////////////////////////////////////////////////////////
+  // printf("Enter a string : ");
+  // gets(str);
   printf("The string-length of %s is : %d\n", str, strlen(str));
   printf("The size of %s is : %d\n", str, sizeof(str));
   return 0;
